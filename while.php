062065701f5c413834c2ab43b7ef1bf8e1eef06f
UtkuDuran <114945016+UtkuDuran@users.noreply.github.com>
2022-10-17 00:28:24 +0300

    Create while.php

---
+++ while.php
@@ -0,0 +1,21 @@
+<?php
+$sehir = [
+"Balıkesir",
+"Kütahya",
+"Bursa",
+"Denizli",
+"İzmir",
+"Manisa",
+"İstanbul",
+"Artvin"
+];
+$i = 0;
+while($i<count($sehir)){
+	echo $sehir[$i];
+	echo "<br/>";
+	$i++;
+
+
+
+}
+?>
